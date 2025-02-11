f5557b4ab43ae38b18a8dd7fcb20a6b3079863df
Your Name <your.email@example.com>
2025-02-10 19:50:44 -0800

    ALRIGHT WE'RE DOING PINPOINT BABY

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/pedroPathing/constants/LConstants.java
@@ -4,22 +4,21 @@ import com.pedropathing.localization.*;
 import com.pedropathing.localization.constants.*;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 public class LConstants {
     static {
-        ThreeWheelIMUConstants.forwardTicksToInches = .001989436789;
-        ThreeWheelIMUConstants.strafeTicksToInches = .001989436789;
-        ThreeWheelIMUConstants.turnTicksToInches = .001989436789;
-        ThreeWheelIMUConstants.leftY = 1;
-        ThreeWheelIMUConstants.rightY = -1;
-        ThreeWheelIMUConstants.strafeX = -2.5;
-        ThreeWheelIMUConstants.leftEncoder_HardwareMapName = "leftFront";
-        ThreeWheelIMUConstants.rightEncoder_HardwareMapName = "rightRear";
-        ThreeWheelIMUConstants.strafeEncoder_HardwareMapName = "rightFront";
-        ThreeWheelIMUConstants.leftEncoderDirection = Encoder.REVERSE;
-        ThreeWheelIMUConstants.rightEncoderDirection = Encoder.REVERSE;
-        ThreeWheelIMUConstants.strafeEncoderDirection = Encoder.FORWARD;
-        ThreeWheelIMUConstants.IMU_HardwareMapName = "imu";
-        ThreeWheelIMUConstants.IMU_Orientation = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.RIGHT);
+        PinpointConstants.forwardY = -7.5;
+        PinpointConstants.strafeX = -5.5;
+        PinpointConstants.distanceUnit = DistanceUnit.INCH;
+        PinpointConstants.hardwareMapName = "pinpoint";
+        PinpointConstants.useYawScalar = false;
+        PinpointConstants.yawScalar = 1.0;
+        PinpointConstants.useCustomEncoderResolution = false;
+        PinpointConstants.encoderResolution = GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD;
+        PinpointConstants.customEncoderResolution = 13.26291192;
+        PinpointConstants.forwardEncoderDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
+        PinpointConstants.strafeEncoderDirection = GoBildaPinpointDriver.EncoderDirection.REVERSED;
     }
 }
 
