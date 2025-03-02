71baad10233d26e69a9273ce47069cc123ef4c09
Your Name <your.email@example.com>
2025-03-02 15:24:46 -0800

    mb

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/StateTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/StateTeleOp.java
@@ -25,7 +25,6 @@ public class StateTeleOp extends LinearOpMode {
         Parts robot = new Parts(hardwareMap);
         StateArm arm = new StateArm();
         GearClaw claw = new GearClaw();
-        PedroDrive drive = new PedroDrive();
 
         Constants.setConstants(FConstants.class, LConstants.class);
         follower = new Follower(hardwareMap);
