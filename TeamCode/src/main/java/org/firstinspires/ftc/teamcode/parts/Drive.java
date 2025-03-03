48c5ef257c6d538748e20173ce40759ad56604a9
Your Name <your.email@example.com>
2025-03-03 13:57:01 -0800

    changed interfaces

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/parts/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/parts/Drive.java
@@ -3,9 +3,9 @@ package org.firstinspires.ftc.teamcode.parts;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 public interface Drive {
-    public void fastSlowSpd(double fast, double slow);
-    public void moveRobot(double x, double y, double yaw);
-    public void feildCentric(Gamepad gamepad);
-    public void robotCentric(Gamepad gamepad);
-    public void tankDrive(Gamepad gamepad);
+    public void fastSlowSpd(double fast, double slow); // setting the fast and slow speeds for drive
+    public void moveRobot(double x, double y, double yaw); // move robot command
+    public void feildCentric(Gamepad gamepad); // field centric drive, condition is the gamepad used
+    public void robotCentric(Gamepad gamepad); // robot centric drive, condition is the gamepad used
+    public void tankDrive(Gamepad gamepad); // tank drive, condition is the gamepad used
 }
